7edbbba1bfca5334a79f0a8371fe62aa13d2a2a9
kiba.x.zhao <kiba.rain@qq.com>
2020-04-20 00:03:21 +0800

    添加ip过滤shell尝试

--- plugins/available/hack.plugin.bash
+++ plugins/available/hack.plugin.bash
@@ -1,10 +1,29 @@
 cite about-plugin
 about-plugin 'hack helper functions'
 
-function testlist() {
-  about 'list hosts defined in ssh config'
-  echo '12313'
-  group 'ssh'
+function scanHost() {
+  about 'explore hosts with nmap scan'
+  group 'hack_explore'
+  # param '1: host'
+  # param '2: hostname'
+  # param '3: user'
 
-  awk '$1 ~ /Host$/ {for (i=2; i<=NF; i++) print $i}' ~/.ssh/config
+  echo 'begin'
+  # local TEST_IP=`ip -4 address | grep -m 1 -E -o '[^0-9]192\.168\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-4]|2[0-4][0-9]|[01]?[0-9][0-9]?)[^0-9]' |awk '{print substr($0,2,length($0)-2)}'`
+  local opt
+  local OPTIND=1
+  local OPTS="-T4 --exclude 192.168.2.108"
+  while getopts "l" opt; do
+    case $opt in
+      l)
+        OPTS="$OPTS -sN"
+        ;;
+    esac
+    echo "after $OPTS"
+    shift $((OPTIND-1))
+  done
+  echo "args: ${@-192.168.2.1/24}"
+  sudo nmap $OPTS ${@-"192.168.2.1/24"};
+  # getopt
+  # nmap 192.168.43.1/24 -sL -S 192.168.43.100
 }
